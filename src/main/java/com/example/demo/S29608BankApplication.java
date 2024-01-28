e82c30b29b0940e722082ce35419f7013b61692d
s29608-pj <dawid.frontczak11@gmail.com>
2024-01-28 12:20:29 +0100

    initial commit

---
+++ src/main/java/com/example/demo/S29608BankApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S29608BankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S29608BankApplication.class, args);
+	}
+
+}
